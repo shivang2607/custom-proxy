2ff410b91089475212cc5fac2659d855a44cf22e
Shivang <shivangkh26@gmail.com>
2024-11-10 16:56:00 +0530

    port changed to 8000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
 // Define a route for fetching data from the target URL
 app.get('/', (req, res) => {
